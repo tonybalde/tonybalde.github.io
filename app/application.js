946c84bbd0c99fc93da4594ca76dfaf7ebca8f0b
tonybalde <tonybalde@hotmail.com>
2022-10-08 21:53:18 -0300

    Porfolio version 1

---
+++ app/application.js
@@ -0,0 +1,37 @@
+// Buttons
+
+function downloadCv()
+{
+     window.open("files/cvAntonioBaldessari.pdf","_blank");
+}
+
+function repoEncriptador()
+{
+     window.open("https://github.com/tonybalde/one-encriptador","_blank");
+}
+
+function demoEncriptador()
+{
+     window.open("https://tonybalde.github.io/one-encriptador/","_blank");
+}
+
+function repoAhorcado()
+{
+     window.open("https://github.com/tonybalde/one-juego-ahorcado","_blank");
+}
+
+function demoAhorcado()
+{
+     window.open("https://tonybalde.github.io/one-juego-ahorcado/ ","_blank");
+}
+
+function repoPorfolio()
+{
+     window.open("https://www.google.com.ar","_blank"); // cambiar por la url del repo del porfolio
+}
+
+function demoPorfolio()
+{
+     window.open("https://www.google.com.ar","_blank");  // cambiar por la url del porfolio
+}
+
