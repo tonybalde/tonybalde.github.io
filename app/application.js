3c4c06a25c7e8c98021e5e64bd9931dbfba9f0cb
tonybalde <tonybalde@hotmail.com>
2022-10-09 22:28:21 -0300

    Add Language Toggle

--- app/application.js
+++ app/application.js
@@ -35,3 +35,83 @@ function demoPorfolio()
      window.open("https://tonybalde.github.io","_blank");
 }
 
+// Languages - translations
+
+var localization = {
+     "es":
+     [
+         {id:"home", text:"Inicio"},
+         {id:"aboutme", text:"Sobre Mí"},
+         {id:"myskills", text:"Skills"},
+         {id:"myhobbies", text:"Hobbies"},
+         {id:"myprojects", text:"Proyectos"},
+         {id:"contactme", text:"Contacto"},
+         {id:"Hello", text:"Hola!"},
+         {id:"MyName", text:"Me llamo Antonio Ernesto Baldessari y soy Desarrollador Front-End."},
+         {id:"download__cv", text:"Descargar CV"},
+         {id:"About__Me", text:"Sobre Mí"},
+         {id:"Paragraph__About__Me", text:"Soy Desarrollador Front-End, con conocimientos en HTML5, CSS3, JavaScript, Git y Github. En búsqueda de trabajo como Front-End Developer."},
+         {id:"movies", text:"Ver Películas y Series"},
+         {id:"music", text:"Escuchar Música"},
+         {id:"running", text:"Salir a Correr"},
+         {id:"guitar", text:"Tocar Guitarra"},
+         {id:"games", text:"Jugar Videojuegos"},
+         {id:"books", text:"Leer Libros"},
+         {id:"Projects", text:"Proyectos"},
+         {id:"Project__One", text:"Encriptador de Texto"},
+         {id:"Project__Two", text:"Juego del Ahorcado"},
+         {id:"Project__Three", text:"Pagina Web Porfolio"},
+         {id:"Contact__Me", text:"Contactame"},
+         {id:"Contact__Me___Paragraph", text:"¿Querés contactarme? Completá el siguiente formulario y me pondré en contacto lo antes posible."},
+         {id:"developed__by", text:"Desarrollado por Antonio E. Baldessari © 2022"}
+
+
+     ],
+     "en": [
+         {id:"home", text:"Home"},
+         {id:"aboutme", text:"About Me"},
+         {id:"myskills", text:"Skills"},
+         {id:"myhobbies", text:"Hobbies"},
+         {id:"myprojects", text:"Projects"},
+         {id:"contactme", text:"Contact"},
+         {id:"Hello", text:"Hello!"},
+         {id:"MyName", text:"My name is Antonio Ernesto Baldessari and I'm a Front-End Developer."},
+         {id:"download__cv", text:"Download Resume"},
+         {id:"About__Me", text:"About Me"},
+         {id:"Paragraph__About__Me", text:"I'm a Front-End Developer, with knowledge in HTML5, CSS3, JavaScript, Git and Github. Looking for a Front-End Developer position."},
+         {id:"movies", text:"Watch Movies and TV Shows"},
+         {id:"music", text:"Listen to Music"},
+         {id:"running", text:"Running"},
+         {id:"guitar", text:"Play Guitar"},
+         {id:"games", text:"Play Videogames"},
+         {id:"books", text:"Read Books"},
+         {id:"Projects", text:"Projects"},
+         {id:"Project__One", text:"Text Encripter"},
+         {id:"Project__Two", text:"Hangman Game"},
+         {id:"Project__Three", text:"Porfolio Webpage"},
+         {id:"Contact__Me", text:"Contact Me"},
+         {id:"Contact__Me___Paragraph", text:"Do you want to contact me? Complete the following form and I will contact you as soon as possible."},
+         {id:"developed__by", text:"Developed by Antonio E. Baldessari © 2022"}
+
+
+     ] 
+   }
+   
+// Functions 
+
+   function toggleLanguage(s)
+   {
+       var lang = s.checked?"en": "es"
+       for(var i = 0; i<localization[lang].length;i++){
+       var a = localization[lang][i];
+         var dom = document.getElementById(a.id)
+         if(dom.text){ 
+           dom.text= a.text
+         }
+         else if(dom.textContent){
+           dom.textContent= a.text
+         }
+       }
+     
+   }
+
